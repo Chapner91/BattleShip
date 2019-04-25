fe4107f0a56b6ea99cf122aa6bb6b9f8134ee156
Charles Hapner <chapner91@gmail.com>
2019-04-25 18:45:01 -0400

    Created new exceptions

---
+++ BattleShip/Exceptions/Exceptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Exceptions
+{
+	class BattleShipException : Exception
+	{
+
+		public BattleShipException() : base("A BattleShip Exception has occurred")
+		{
+			
+		}
+
+		public BattleShipException(string message) : base(message)
+		{
+
+		}
+	}
+
+	class ShipException : BattleShipException
+	{
+		public ShipException() : base("A Ship Exception has occurred")
+		{
+
+		}
+
+		public ShipException(string message) : base(message)
+		{
+
+		}
+	}
+
+	class ShipPartException : BattleShipException
+	{
+		public ShipPartException() : base("A ShipPart Exception has occurred")
+		{
+
+		}
+
+		public ShipPartException(string message) : base(message)
+		{
+
+		}
+
+
+	}
+}
